cca1b1e01d43439a4c3286d6fd2585516760a28b
thsamajiki <ant1102@naver.com>
2023-07-30 15:13:20 +0900

    메인 화면 업로드

---
+++ app/src/main/java/com/dnd_9th_3_android/gooding/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dnd_9th_3_android.gooding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
