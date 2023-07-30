ec1a7d1f54ffc4c44ac0549baa97b39ff1e3628d
thsamajiki <ant1102@naver.com>
2023-07-30 15:01:22 +0900

    gradle 초기 설정

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    @Suppress("UnstableApiUsage")
+    repositories {
+        google()
+        mavenCentral()
+        maven { setUrl("https://devrepo.kakao.com/nexus/content/groups/public/") }
+    }
+}
+rootProject.name = "Gooding"
+include(":app")
