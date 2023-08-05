6ec1ae89b91d42d684077676186b809067e6d68f
김진우 <jinwoo3661@naver.com>
2023-08-06 01:33:50 +0900

    feature/feed dimens 파일 추가

--- feature/feed/src/main/java/com/dnd_9th_3_android/gooding/feed/FeedScreen.kt
+++ feature/feed/src/main/java/com/dnd_9th_3_android/gooding/feed/FeedScreen.kt
@@ -1,15 +1,20 @@
 package com.dnd_9th_3_android.gooding.feed
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun FeedScreen() {
-    Box(
-        contentAlignment = Alignment.Center
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxHeight().fillMaxWidth()
     ){
         Text(text = "main feed")
     }
