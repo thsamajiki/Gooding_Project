3d43d786d8ae7d99fcef7699542bb0185396cc66
thsamajiki <ant1102@naver.com>
2023-08-08 00:45:39 +0900

    프래그먼트 이름 수정

--- app/src/main/java/com/dnd_9th_3_android/gooding/MyGoodingFragment.kt
+++ app/src/main/java/com/dnd_9th_3_android/gooding/MyGoodingFragment.kt
@@ -6,17 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class MyPageFragment : Fragment() {
+class MyGoodingFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+        return inflater.inflate(R.layout.fragment_my_gooding, container, false)
     }
 
     companion object {
-        fun newInstance() = MyPageFragment()
+        fun newInstance() = MyGoodingFragment()
     }
 }
