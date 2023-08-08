198271e2e610273b82ecee031b448a406ae7a0b0
thsamajiki <ant1102@naver.com>
2023-08-08 23:09:55 +0900

    갤러리 로컬 데이터 소스 함수 추가

--- app/src/main/java/com/dnd_9th_3_android/gooding/data/GalleryLocalDataSource.kt
+++ app/src/main/java/com/dnd_9th_3_android/gooding/data/GalleryLocalDataSource.kt
@@ -4,4 +4,6 @@ import com.dnd_9th_3_android.gooding.data.model.GalleryImageData
 
 interface GalleryLocalDataSource {
     suspend fun getAllImages(page: Int): List<GalleryImageData>
+
+//    suspend fun fetchGalleryImages(limit: Int, offset: Int): List<GalleryImageData>
 }
--- app/src/main/java/com/dnd_9th_3_android/gooding/data/GalleryLocalDataSourceImpl.kt
+++ app/src/main/java/com/dnd_9th_3_android/gooding/data/GalleryLocalDataSourceImpl.kt
@@ -5,6 +5,8 @@ import android.content.ContentUris
 import android.content.Context
 import android.database.Cursor
 import android.net.Uri
+import android.os.Build
+import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import com.dnd_9th_3_android.gooding.data.model.GalleryImageData
@@ -38,7 +40,7 @@ class GalleryLocalDataSourceImpl @Inject constructor(
             MediaStore.Images.Media.HEIGHT,
         )
 
-        val selection: String? = null
+        val selection: String? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) MediaStore.Images.Media.SIZE + " > 0" else null
         val selectionArgs: Array<String>? = null
 
         cursor = resolver?.query(
@@ -74,4 +76,79 @@ class GalleryLocalDataSourceImpl @Inject constructor(
 
         return galleryImageList
     }
+
+//    override suspend fun fetchGalleryImages(limit: Int, offset: Int): List<GalleryImageData> {
+//        val contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+//        val projection = arrayOf(
+//            MediaStore.Images.Media._ID,
+//            MediaStore.Images.Media.TITLE,
+//            MediaStore.Images.Media.DISPLAY_NAME,
+//            MediaStore.Images.Media.MIME_TYPE,
+//            MediaStore.Images.Media.DATE_TAKEN,
+//            MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
+//            MediaStore.Images.Media.SIZE,
+//            MediaStore.Images.Media.WIDTH,
+//            MediaStore.Images.Media.HEIGHT,
+//        )
+//        val galleryImage = mutableListOf<GalleryImageData>()
+//        val selection =
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) MediaStore.Images.Media.SIZE + " > 0"
+//            else null
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//            resolver?.query(
+//                contentUri,
+//                projection,
+//                Bundle().apply {
+//                    // Limit & Offset
+//                    putInt(ContentResolver.QUERY_ARG_LIMIT, limit)
+//                    putInt(ContentResolver.QUERY_ARG_OFFSET, offset)
+//
+//                    // Sort function
+//                    putStringArray(
+//                        ContentResolver.QUERY_ARG_SORT_COLUMNS,
+//                        arrayOf(MediaStore.Images.Media.DATE_TAKEN)
+//                    )
+//                    putInt(
+//                        ContentResolver.QUERY_ARG_SORT_DIRECTION,
+//                        ContentResolver.QUERY_SORT_DIRECTION_DESCENDING
+//                    )
+//
+//                    // Selection
+//                    putString(ContentResolver.QUERY_ARG_SQL_SELECTION, selection)
+//                }, null
+//            )
+//        } else {
+//            val sortOrder =
+//                "${MediaStore.Images.Media.DATE_TAKEN} DESC LIMIT $limit OFFSET $offset"
+//            resolver?.query(
+//                contentUri,
+//                projection,
+//                selection,
+//                null,
+//                sortOrder
+//            )
+//        }?.use { cursor ->
+//            while (cursor.moveToNext()) {
+//                galleryImage.add(
+//                    GalleryImageData(
+//                        uri = Uri.withAppendedPath(
+//                            contentUri,
+//                            cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)).toString()
+//                        ),
+//                        name = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.TITLE)),
+//                        fullName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)),
+//                        mimeType = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.MIME_TYPE)),
+//                        addedDate = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATE_TAKEN)),
+//                        folder = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME)),
+//                        size = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.SIZE)),
+//                        width = cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.WIDTH)),
+//                        height = cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.HEIGHT)),
+//                    )
+//                )
+//            }
+//        }
+//
+//        return galleryImage
+//    }
 }
