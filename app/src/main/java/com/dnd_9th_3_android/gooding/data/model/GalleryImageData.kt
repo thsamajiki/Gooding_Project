a9b4dfe74e43f2ba60a7c61944b7b5db1cb69686
thsamajiki <ant1102@naver.com>
2023-08-08 22:32:56 +0900

    갤러리 이미지 데이터 수정

--- app/src/main/java/com/dnd_9th_3_android/gooding/data/model/GalleryImageData.kt
+++ app/src/main/java/com/dnd_9th_3_android/gooding/data/model/GalleryImageData.kt
@@ -6,13 +6,13 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class GalleryImageData(
-    val uri: Uri,
-    val name: String,
-    val fullName: String,
-    val mimeType: String,
-    val addedDate: Long,
-    val folder: String,
-    val size: Long,
-    val width: Int,
-    val height: Int
+    val uri: Uri,           // 이미지 Uri
+    val name: String,       // 이름
+    val fullName: String,   // 확장자를 포함한 이름
+    val mimeType: String,   // Mime 형식의 type
+    val addedDate: Long,    // 저장된 날짜
+    val folder: String,     // 저장된 폴더 명
+    val size: Long,         // 파일의 크기
+    val width: Int,         // 사진의 너비
+    val height: Int         // 사진의 높이
 ): Parcelable
