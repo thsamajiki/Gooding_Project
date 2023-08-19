3fdde45e64eda1e267ee38356f9fa74086d25a88
thsamajiki <ant1102@naver.com>
2023-08-19 20:57:18 +0900

    커스텀 갤러리 데이터 불러오는 로직 수정

--- app/src/main/java/com/dnd_9th_3_android/gooding/data/GalleryLocalDataSource.kt
+++ app/src/main/java/com/dnd_9th_3_android/gooding/data/GalleryLocalDataSource.kt
@@ -1,9 +1,15 @@
 package com.dnd_9th_3_android.gooding.data
 
+import com.dnd_9th_3_android.gooding.data.model.GalleryData
 import com.dnd_9th_3_android.gooding.data.model.GalleryImageData
+import com.dnd_9th_3_android.gooding.data.model.GalleryVideoData
 
 interface GalleryLocalDataSource {
     fun getAllImages(page: Int): List<GalleryImageData>
+
+    fun getAllVideos(page: Int): List<GalleryVideoData>
+
+    fun getImageVideoFromGallery(): List<GalleryData>
 
 //    suspend fun fetchGalleryImages(limit: Int, offset: Int): List<GalleryImageData>
 }
